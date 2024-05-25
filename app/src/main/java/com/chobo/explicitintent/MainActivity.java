376ec7eaf243db6da5b74680f17e489914a09ae9
imi2080 <imi2080@gmail.com>
2024-05-25 21:46:18 +0900

    EdgeToEdge.enable 삭제

--- app/src/main/java/com/chobo/explicitintent/MainActivity.java
+++ app/src/main/java/com/chobo/explicitintent/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.layout1);
         Button b = (Button) findViewById(R.id.Button01);
         b.setOnClickListener(new View.OnClickListener() {
